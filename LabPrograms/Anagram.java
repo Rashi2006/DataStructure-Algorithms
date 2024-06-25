7d4382b9aee8193e1c0d800cd3de78166bc22a0a
Rashi2006 <140819546+Rashi2006@users.noreply.github.com>
2024-06-25 18:41:20 +0530

    ANAGRAM GAME

---
+++ LabPrograms/Anagram.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of words:");
+        int n=sc.nextInt();
+        System.out.println("ENter the words:");
+        String[] str=new String[n];
+        for(int i=0;i<n;i++){
+            System.out.println("word"+i+"=");
+            str[i]=sc.next();
+        }
+        int range=(n-1)-0+1;
+        int rand=(int)(Math.random()*range);
+        System.out.println("The randomly generated word is:"+str[rand]);
+        System.out.println("Enter the anagram of given word:");
+        String s=sc.next();
+        boolean isFound=false;
+        for(int i=0;i<str[rand].length();i++){
+            isFound=false;
+            for(int j=0;j<s.length();j++){
+                if(str[rand].charAt(i)==s.charAt(j)){
+                    isFound=true;
+                }
+            }
+            if(!isFound){
+                break;
+            }
+        }
+        if(isFound){
+            System.out.println("Anagram");
+        }else{
+            System.out.println("Not an anagram");
+        }
+    }
+}
